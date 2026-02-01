dfb45c2ec60a83447b4bee773fcebd42490e4450
Bolton Bailey <bolton.bailey@gmail.com>
2026-02-01 14:55:36 -0800

    fix doc

--- Mathlib/AlgebraicTopology/SimplicialComplex/Basic.lean
+++ Mathlib/AlgebraicTopology/SimplicialComplex/Basic.lean
@@ -143,10 +143,11 @@ instance : CompleteLattice (PreAbstractSimplicialComplex ι) where
 
 end PreAbstractSimplicialComplex
 
-@[ext]
+
 /--
 An `AbstractSimplicialComplex` is a `PreAbstractSimplicialComplex` which contains all singletons.
 -/
+@[ext]
 structure AbstractSimplicialComplex extends PreAbstractSimplicialComplex ι where
   /-- every singleton is a face -/
   singleton_mem : ∀ v : ι, {v} ∈ faces
