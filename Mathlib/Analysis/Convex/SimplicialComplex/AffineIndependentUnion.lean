b22cd4c7a2a06e85b1dcf661cd772755d090f421
Bolton Bailey <bolton.bailey@gmail.com>
2026-02-09 12:54:13 -0800

    add simp lemma to golf

--- Mathlib/Analysis/Convex/SimplicialComplex/AffineIndependentUnion.lean
+++ Mathlib/Analysis/Convex/SimplicialComplex/AffineIndependentUnion.lean
@@ -40,13 +40,10 @@ are 0-simplices and edges are 1-simplices.
 -/
 def ofSimpleGraph {ι : Type*} [DecidableEq ι] (G : SimpleGraph ι) :
     AbstractSimplicialComplex ι where
-  faces := ((fun v => ({v} : Finset ι)) '' (Set.univ (α := ι))) ∪ Sym2.toFinset '' G.edgeSet
-  empty_notMem := by
-    simp only [Set.mem_union, Set.mem_image, Set.mem_univ, true_and, Finset.singleton_ne_empty,
-      exists_false, false_or, not_exists, not_and]
-    exact fun _ _ h => Finset.ne_empty_of_mem (Sym2.mem_toFinset.mpr (Sym2.out_fst_mem _)) h
+  faces := ({s : Finset ι | ∃ v, s = {v}}) ∪ Sym2.toFinset '' G.edgeSet
+  empty_notMem := by simp
   down_closed := by
-    simp only [Set.mem_union, Set.mem_image, Set.mem_univ, true_and]
+    simp only [Set.mem_union, Set.mem_setOf_eq, Set.mem_image]
     intro s t hs hts ht
     rcases hs with ⟨v, rfl⟩ | ⟨e, he, rfl⟩
     · simp only [Finset.subset_singleton_iff] at hts
@@ -56,8 +53,8 @@ def ofSimpleGraph {ι : Type*} [DecidableEq ι] (G : SimpleGraph ι) :
     · by_cases hc : t.card ≤ 1
       · left
         obtain ⟨x, hx⟩ := ht
-        exact ⟨x, (Finset.eq_singleton_iff_unique_mem.mpr
-          ⟨hx, fun y hy => Finset.card_le_one.mp hc y hy x hx⟩).symm⟩
+        exact ⟨x, Finset.eq_singleton_iff_unique_mem.mpr
+          ⟨hx, fun y hy => Finset.card_le_one.mp hc y hy x hx⟩⟩
       · right
         push_neg at hc
         have hle : e.toFinset.card ≤ t.card := by
@@ -65,7 +62,7 @@ def ofSimpleGraph {ι : Type*} [DecidableEq ι] (G : SimpleGraph ι) :
           split_ifs at this <;> omega
         exact ⟨e, he, (Finset.eq_of_subset_of_card_le hts hle).symm⟩
   singleton_mem := by
-    simp only [Set.mem_union, Set.mem_image, Set.mem_univ, true_and]
+    simp only [Set.mem_union, Set.mem_setOf_eq, Set.mem_image]
     intro v
     exact Or.inl ⟨v, rfl⟩
 
